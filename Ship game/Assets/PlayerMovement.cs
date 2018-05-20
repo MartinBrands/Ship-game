30e4caa1e0a93c145f4726dbd95618bf06b5d5fa
Martin Brandstetter <martinbran94@gmail.com>
2018-05-19 21:16:45 -0300

    PlayerMovement

---
+++ Ship game/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Transform playerPosition;
+    private Vector3 moveUp = new Vector3(10, 0, 10);
+    private Vector3 moveDown = new Vector3(-10, 0, -10);
+    private Vector3 moveLeft = new Vector3(-10, 0, 10);
+    private Vector3 moveRight = new Vector3(10, 0, -10);
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        if(Input.GetKey(KeyCode.UpArrow) )
+        {
+            transform.Translate(moveUp * Time.deltaTime);
+            //playerPosition.position = playerPosition.position + moveUp * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(moveDown * Time.deltaTime);
+            //playerPosition.position = playerPosition.position + moveDown * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(moveLeft * Time.deltaTime);
+            //playerPosition.position = playerPosition.position + moveLeft * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(moveRight * Time.deltaTime);
+            //playerPosition.position = playerPosition.position + moveRight * Time.deltaTime;
+        }
+
+    }
+}
